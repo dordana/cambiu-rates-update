2a8e65875ab02a290748a9a3cc47b6c0f5131a28
dor <dordanaa@gmail.com>
2017-09-12 10:04:37 +0000

    test

--- run.js
+++ run.js
@@ -9,6 +9,10 @@
 └───────────────────────── second (0 - 59, OPTIONAL)
 */
 //CronJob(cronTime, callback, [args], [context], [repeating = true])
+
+
+var http = require('http');
+http.createServer(function (request, response) {}).listen(process.env.PORT || 5000);
 const acc = 'AC30f9cba26999974ebfc6a3bac2cf82b7';
 const id = '03365315d1ca59368bc7b3b633bb801d';
 const   client = require("twilio")(acc,id),
@@ -19,7 +23,7 @@ var hours = 0;
 var existelms = [];
 var existelmsdone = [];
 var CronJob = require('cron').CronJob;
-var job = new CronJob('00 00 * * * 0-6', function() {
+// var job = new CronJob('00 00 * * * 0-6', function() {
     var dateNtime= moment.tz("Asia/Jerusalem").format('DD/MM/YYYY HH:mm:ss');
     
     console.log("******************************************************************************************************************************************")
@@ -37,18 +41,18 @@ var job = new CronJob('00 00 * * * 0-6', function() {
         console.log("************************************************** Finished! - "+dateNtime+" *********************************************************")
         console.log("******************************************************************************************************************************************")
     }); 
-  },true).start();
+//   },true).start();
         
     
-var jobdaily = new CronJob('00 05 20 * * 0-6', function() {
-    sendEmailReport(createmailreport()).then(function(res){
-                console.log(res);
-            }).catch(function(res){
-                console.error(res);
-            })
-            existelms = [];
-            existelmsdone = [];
-},true).start();
+// var jobdaily = new CronJob('00 05 20 * * 0-6', function() {
+//     sendEmailReport(createmailreport()).then(function(res){
+//                 console.log(res);
+//             }).catch(function(res){
+//                 console.error(res);
+//             })
+//             existelms = [];
+//             existelmsdone = [];
+// },true).start();
 
 
 
@@ -65,6 +69,7 @@ function sendEmailReport(repText)
     {
         from: 'Cambiu - Update rates report <postmaster@sandbox3fc985a1f4274f558f5239547f7a9c33.mailgun.org>',
         to: 'dor@cambiu.com, dror@cambiu.com, eyal@cambiu.com, yonatan@cambiu.com',
+        // to: 'dor@cambiu.com',
         subject: 'Cambiu - Update rates report - '+ dateNtime,
         html: repText
     };
@@ -103,13 +108,13 @@ function sendSMSReport(Report)
         from: "+17868863180",
         body: ' \r\n['+dateNtime+']\r\n *Update report*\r\n' + 'Number of success: ' + Report.numberOfSuccess  + '\r\nNumber of failed: ' + Report.numberOfFailed
     });
-    console.log("Sending a message to +972542343220");
-    client.messages.create
-    ({
-        to: "+972542343220",
-        from: "+17868863180",
-        body: ' \r\n['+dateNtime+']\r\n *Update report*\r\n' + 'Number of success: ' + Report.numberOfSuccess  + '\r\nNumber of failed: ' + Report.numberOfFailed
-    });
+    // console.log("Sending a message to +972542343220");
+    // client.messages.create
+    // ({
+    //     to: "+972542343220",
+    //     from: "+17868863180",
+    //     body: ' \r\n['+dateNtime+']\r\n *Update report*\r\n' + 'Number of success: ' + Report.numberOfSuccess  + '\r\nNumber of failed: ' + Report.numberOfFailed
+    // });
 }
 
 function createmailreport()
