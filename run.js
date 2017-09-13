9e347b6cae0020f7761e023597921e02ffbdc21c
dor <dordanaa@gmail.com>
2017-09-13 08:31:32 +0000

    fix bug

--- run.js
+++ run.js
@@ -23,7 +23,7 @@ var hours = 0;
 var existelms = [];
 var existelmsdone = [];
 var CronJob = require('cron').CronJob;
-var job = new CronJob('00 15 * * * 0-6', function() {
+var job = new CronJob('00 00 * * * 0-6', function() {
     var dateNtime= moment.tz("Asia/Jerusalem").format('DD/MM/YYYY HH:mm:ss');
     
     console.log("******************************************************************************************************************************************")
@@ -110,13 +110,13 @@ function sendSMSReport(Report)
         from: "+17868863180",
         body: ' \r\n['+dateNtime+']\r\n *Test - Update report*\r\n' + 'Number of success: ' + Report.numberOfSuccess  + '\r\nNumber of failed: ' + Report.numberOfFailed
     });
-    // console.log("Sending a message to +972542343220");
-    // client.messages.create
-    // ({
-    //     to: "+972542343220",
-    //     from: "+17868863180",
-    //     body: ' \r\n['+dateNtime+']\r\n *Update report*\r\n' + 'Number of success: ' + Report.numberOfSuccess  + '\r\nNumber of failed: ' + Report.numberOfFailed
-    // });
+    console.log("Sending a message to +972542343220");
+    client.messages.create
+    ({
+        to: "+972542343220",
+        from: "+17868863180",
+        body: ' \r\n['+dateNtime+']\r\n *Update report*\r\n' + 'Number of success: ' + Report.numberOfSuccess  + '\r\nNumber of failed: ' + Report.numberOfFailed
+    });
 }
 
 function createmailreport()
