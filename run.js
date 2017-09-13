8b5bfec7b22c96e8460b5e528a61deb7775be0ec
dor <dordanaa@gmail.com>
2017-09-13 23:10:36 +0000

    v1.5

--- run.js
+++ run.js
@@ -22,9 +22,8 @@ var emailtemp = "<p style=\"text-align: center;\"><strong><img src=\"http://join
 var existelms = [];
 var existelmsdone = [];
 var CronJob = require('cron').CronJob;
-var job = new CronJob('0 */5 * * * 0-6', function() {
+var job = new CronJob('0 0 * * * 0-6', function() {
     var dateNtime= moment.tz("Asia/Jerusalem").format('DD/MM/YYYY HH:mm:ss');
-    
     console.log("******************************************************************************************************************************************")
     console.log("******************************************************** Start working - "+dateNtime+" version 1.2 ************************************************")
     console.log("******************************************************************************************************************************************\r\n")
@@ -109,13 +108,13 @@ function sendSMSReport(Report)
         from: "+17868863180",
         body: '['+dateNtime+']\r\n *Update report*\r\n' + Report
     });
-    // console.log("Sending a message to +972542343220");
-    // client.messages.create
-    // ({
-    //     to: "+972542343220",
-    //     from: "+17868863180",
-    //     body: '['+dateNtime+']\r\n *Update report*\r\n' + Report
-    // });
+    console.log("Sending a message to +972542343220");
+    client.messages.create
+    ({
+        to: "+972542343220",
+        from: "+17868863180",
+        body: '['+dateNtime+']\r\n *Update report*\r\n' + Report
+    });
 }
 
 function createSMSreport(report){
@@ -182,3 +181,5 @@ function createmailreport()
     }
     return failedRep;
 }
+
+
